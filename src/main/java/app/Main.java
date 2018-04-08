6fd9cace907082a4e57984d17aa8689a46343968
as <qnas2426@gmail.com>
2018-04-08 21:46:07 +0300

    214213

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) { ;
+    public static void main(String[] args) {
 
         System.out.println("---------------- بداية عملية الجمع----------------");
         System.out.println("نتيجة جمع الرقمين تساوي " + getSum());
@@ -13,6 +13,14 @@ public class Main {
         System.out.println("---------------- بداية عملية الطرح----------------");
         System.out.println("نتيجة طرح الرقمين تساوي " + getSubtract());
         System.out.println("----------------نهاية عملية الطرح----------------");
+
+        System.out.println("---------------- بداية عملية القسمة----------------");
+        System.out.println("نتيجة قسمة الرقمين تساوي " + qsma());
+        System.out.println("----------------نهاية عملية القسمة----------------");
+
+        System.out.println("---------------- بداية عملية الضرب----------------");
+        System.out.println("نتيجة الضرب الرقمين تساوي " + dreb ());
+        System.out.println("----------------نهاية عملية الضرب----------------");
 
     }
 
@@ -38,5 +46,29 @@ public class Main {
         double num2 = input.nextDouble();
 
         return num1 - num2;
+    }
+
+    public static double qsma () {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("ادخل الرقم الأ,ل:");
+        double num1 = input.nextDouble();
+
+        System.out.println("ادخل الرقم الثاني:");
+        double num2 = input.nextDouble();
+
+        return num1 / num2;
+    }
+
+        public static double dreb (){
+            Scanner input = new Scanner(System.in);
+
+            System.out.println("ادخل الرقم الأ,ل:");
+            double num1 = input.nextDouble();
+
+            System.out.println("ادخل الرقم الثاني:");
+            double num2 = input.nextDouble();
+
+            return num1 * num2;
     }
 }
