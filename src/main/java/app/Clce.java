ca78c3b5237b541412018d0299cc27cc98fc544d
as <qnas2426@gmail.com>
2018-04-08 22:49:29 +0300

    شرح المعاملات الحسابية x++ للجمع الرقم عن طريق وطريقة قيمة الكس والواي اكبر واصغر او لاتساوي

--- src/main/java/app/Clce.java
+++ src/main/java/app/Clce.java
@@ -6,10 +6,25 @@ public class Clce {
 
         // طريقة عمل المعاملات الحسابية
 
-        int x  = 117;
-        x++;
+       /* int x  = 117;
+       * x++;
 
-        System.out.println( ++x );
+      *  System.out.println( ++x );*/
+       //طريقة عمل ان قيم اكس وقيمة واي تساوي القيمتين  بناتج بصح او غلط !!
+
+       int x =20;
+       int y =20;
+       //طريقة عملية ان قيمة الكس والواي كما هو مكتوب ادناه
+
+       System.out.println(x==y);
+       System.out.println(x!=y);
+       System.out.println(x > y);
+        System.out.println(x < y);
+        System.out.println(x >= y);
+        System.out.println(x<=y);
+
+
+
 
     }
 }
