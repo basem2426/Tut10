2764e291a5dd54c89f8395ef52b09e0ee7d2783d
as <qnas2426@gmail.com>
2018-04-29 22:49:37 +0300

    1

---
+++ src/main/java/app/Car.java
@@ -0,0 +1,22 @@
+package app;
+
+
+public class Car {
+
+    int id;
+    String model;
+    String name;
+
+
+    public Car( int outerid, String outerModel, String outerName ) {
+        id = outerid;
+        model = outerModel;
+        name = outerName ;
+
+    }
+
+}
+
+
+
+
---
+++ src/main/java/app/CarTest.java
@@ -0,0 +1,23 @@
+package app;
+
+public class CarTest {
+    public static void main(String[] arges) {
+
+       Car marcedes3 = new Car(10,"@","marcedes3");
+       System.out.println(marcedes3.name);
+
+
+
+
+
+//        System.out.println(" اسم السيارة  " + mercedes.name);
+//        mercedes.name = " maa ";
+//        System.out.println(" اسم السيارة  " + mercedes.name);
+
+
+
+
+
+    }
+
+}
